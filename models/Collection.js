9fab42c13e87a17511726718a2e2f24e9707d780
Tobias Westphal <61382793+tobidwest@users.noreply.github.com>
2025-05-20 21:34:43 +0200

    Added all necessary API routes

--- models/Collection.js
+++ models/Collection.js
@@ -24,10 +24,23 @@ const collectionSchema = new Schema(
         ref: "Link",
       },
     ],
+    shareId: {
+      type: String,
+      unique: true,
+      required: true,
+      index: true,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+collectionSchema.pre("validate", function (next) {
+  if (!this.shareId) {
+    this.shareId = crypto.randomBytes(32).toString("hex");
+  }
+  next();
+});
 
 module.exports = mongoose.model("Collection", collectionSchema);
